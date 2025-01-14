daeb1c62a5d65f6913ddcae96ba5721fc20e4438
Hafiz Amsal Ali <hafizamsalali1@gmail.com>
2025-01-14 12:12:13 +0100

    Add app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+git config --global user.name "Your Name"
+git config --global user.email "youremail@example.com"
+git config --list
+
+
+git clone <link>
+
+
+git commit -m "Your message"
+
+
+git push origin main
+
+#write me aprogram that prints me the number of seconds in an year
+#write a program that prints the number of days in a year
